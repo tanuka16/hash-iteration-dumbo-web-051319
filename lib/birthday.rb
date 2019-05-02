a77ba40d2a1a9197cef50c1d63adae96d5eaf25f
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 22:18:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |name, age|
+  birthday_kids.each do |kids_name, age|
     print "Happy Birthday #{kids_name}! You are now #{age} years old!"
 
   end
