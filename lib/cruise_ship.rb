5b09615150647f7fb58b519009cc2d6343c1e932
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 22:27:34 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,24 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = " "
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")           #boolean operator to check if we have the right suite and if the person in that suite has a name that begins with the letter "A"
+      winner = name
+    end
+  end
+  winner                                                    #call on winner variable to return the name of the lucky winner
 end
+
+
+#We iterate through the hash using #each. We chose #each instead of collect
+#because we don't want to collect the key/value pair that contains the winner,
+#just the name of the winner. With #each, we have the control we need to simply grab
+#the winner's name and set it equal to a variable that we can return later on.
