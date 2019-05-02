32bf2b5262e57d9ed3b0187bef0e11f6d4c19984
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 22:13:12 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,3 +16,9 @@ def select_winner(passengers)
   end
   winner                                                    #call on winner variable to return the name of the lucky winner
 end
+
+
+#We iterate through the hash using #each. We chose #each instead of collect
+#because we don't want to collect the key/value pair that contains the winner,
+#just the name of the winner. With #each, we have the control we need to simply grab
+#the winner's name and set it equal to a variable that we can return later on.
