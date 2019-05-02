36b85c6554e8f1097296353c160328f2d3878405
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 22:12:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,9 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = " "
   passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")           #boolean operator to check if we have the right suite and if the person in that suite has a name that begins with the letter "A"
       winner = name
     end
   end
-  winner
+  winner                                                    #call on winner variable to return the name of the lucky winner
 end
